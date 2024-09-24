17b3194ed33fcd09b1bcbf9089bc22334350bc70
Rafael de Souza Sell <mcsaitocanal@gmail.com>
2024-09-24 13:54:33 -0300

    fixed image overflow

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -14,22 +14,15 @@ export const Layout = ({ children }: LayoutProps) => {
       w={"100%"}
       bgColor={"brand.black.100"}
       align={"center"}
+      bgImage={"bgeffect.svg"}
+      bgSize={"cover"}
+      bgPos={"center"}
     >
       <Stack align={"center"} minH={"100vh"} maxW={"1200px"} w={"100%"}>
         <Navbar />
         {children}
         <Footer />
       </Stack>
-      <Image
-        pointerEvents={"none"}
-        zIndex={0}
-        overflow={"hidden"}
-        objectFit={"cover"}
-        src="/bgeffect.svg"
-        alt="bgeffect"
-        position={"absolute"}
-        top={70}
-      />
     </Stack>
   );
 };
